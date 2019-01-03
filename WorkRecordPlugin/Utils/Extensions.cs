28c726177e5ecab9c422d6d16d1cf870b98eff34
Jason Roesbeke <jason.roesbeke@cnhind.com>
2019-01-03 18:09:48 +0100

    Utils to convert Int64 & Int32 to and from Guid

---
+++ WorkRecordPlugin/Utils/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkRecordPlugin.Utils
+{
+	static class Extensions
+	{
+		public static Guid IntToGuid(this int value)
+		{
+			byte[] bytes = new byte[16];
+			BitConverter.GetBytes(value).CopyTo(bytes, 0);
+			return new Guid(bytes);
+		}
+
+		public static int GuidToInt(this Guid value)
+		{
+			byte[] b = value.ToByteArray();
+			int bint = BitConverter.ToInt32(b, 0);
+			return bint;
+		}
+
+		public static Guid LongToGuid(this long value)
+		{
+			byte[] bytes = new byte[16];
+			BitConverter.GetBytes(value).CopyTo(bytes, 0);
+			return new Guid(bytes);
+		}
+
+		public static Int64 GuidToLong(this Guid value)
+		{
+			byte[] b = value.ToByteArray();
+			long bint = BitConverter.ToInt64(b, 0);
+			return bint;
+		}
+	}
+}
