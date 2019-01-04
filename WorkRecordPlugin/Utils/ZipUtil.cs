4b5f320084b136bb9dad23e88aa7997d433f49d3
Jason Roesbeke <jason.roesbeke@cnhind.com>
2019-01-04 15:15:54 +0100

    Adding ZipUtil, copied from ADM plugin

---
+++ WorkRecordPlugin/Utils/ZipUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace WorkRecordPlugin.Utils
+{
+	public static class ZipUtil
+	{
+		public static void Zip(string file, string tempFile)
+		{
+			using (var openTempStream = File.Open(tempFile, FileMode.Open))
+			using (var admFileStream = File.Open(file, FileMode.Create, FileAccess.ReadWrite))
+			using (var gzipStream = new GZipStream(admFileStream, CompressionLevel.Optimal))
+			{
+				openTempStream.CopyTo(gzipStream);
+			}
+		}
+
+		public static void Unzip(string file, string tempFile)
+		{
+			using (var openStream = File.Open(file, FileMode.Open))
+			using (var admStream = File.Open(tempFile, FileMode.Create, FileAccess.ReadWrite))
+			using (var gzipStream = new GZipStream(openStream, CompressionMode.Decompress))
+			{
+				gzipStream.CopyTo(admStream);
+			}
+		}
+	}
+}
