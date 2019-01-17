7834805b1158671b7234655b1b5c784d9ebd4d53
Jason Roesbeke <jason.roesbeke@cnhind.com>
2019-01-17 15:33:08 +0100

    Adding LicenseHeader

--- WorkRecordPlugin/Models/DTOs/ADAPT/Logistics/FieldDto.cs
+++ WorkRecordPlugin/Models/DTOs/ADAPT/Logistics/FieldDto.cs
@@ -1,4 +1,15 @@
-﻿using GeoJSON.Net.Feature;
+﻿/*******************************************************************************
+  * Copyright (C) 2019 AgGateway and ADAPT Contributors
+  * Copyright (C) 2019 CNH Industrial N.V.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v1.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v20.html
+  *
+  * Contributors:
+  *    Jason Roesbeke - Initial version.
+  *******************************************************************************/
+using GeoJSON.Net.Feature;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
--- WorkRecordPlugin/Models/DTOs/ADAPT/Representations/AdditionalRepresentations.cs
+++ WorkRecordPlugin/Models/DTOs/ADAPT/Representations/AdditionalRepresentations.cs
@@ -1,4 +1,15 @@
-﻿using AgGateway.ADAPT.ApplicationDataModel.Representations;
+﻿/*******************************************************************************
+  * Copyright (C) 2019 AgGateway and ADAPT Contributors
+  * Copyright (C) 2019 CNH Industrial N.V.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v1.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v20.html
+  *
+  * Contributors:
+  *    Jason Roesbeke - Initial version.
+  *******************************************************************************/
+using AgGateway.ADAPT.ApplicationDataModel.Representations;
 using System;
 using System.Collections.Generic;
 using System.Text;
