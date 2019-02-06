f630d057d09dc6e0afa85f8dc3e202ffe8bfda59
Jason Roesbeke <jason.roesbeke@cnhind.com>
2019-02-06 09:18:13 +0100

    Adding DeviceModelDto

---
+++ WorkRecordPlugin/Models/DTOs/ADAPT/Equipment/DeviceModelDto.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkRecordPlugin.Models.DTOs.ADAPT.Equipment
+{
+	public class DeviceModelDto : BaseDto
+	{
+
+		public DeviceModelDto(string Parent) : base(Parent, "Brand", "Manufacturer")
+		{
+		}
+
+		public DeviceModelDto() : base(null, "Brand", "Manufacturer")
+		{
+		}
+
+		[JsonProperty(PropertyName = EntityId, Order = -2)]
+		public Guid Guid { get; set; }
+
+		[JsonProperty(Required = Required.Always, Order = -1)]
+		public string Description { get; set; }
+
+		//[JsonIgnore] // ADAPT 2.0.1
+		//public string Manufacturer { get; set; }
+
+		public string Brand { get; set; }
+
+		[JsonIgnore] // ADAPT 2.0.1
+		public string Series { get; set; }
+	}
+}
