450823a2ece2ecbda8731e7813badcc147570e5c
Jason Roesbeke <jason.roesbeke@cnhind.com>
2020-06-09 18:47:46 +0200

    Moved and 'unflatten' the multipolygon mapper to mappers for the following GeoJSONobjectTypes: Point, LineString, Polygon & MultiPolygon. Note: these are now in a new namespace WorkRecordPlugin.Mappers.GeoJson. Only from ADAPT to GeoJSon currently.

---
+++ WorkRecordPlugin/Mappers/GeoJson/MultiPolygonMapper.cs
@@ -0,0 +1,61 @@
+﻿/*******************************************************************************
+  * Copyright (C) 2019 AgGateway and ADAPT Contributors
+  * Copyright (C) 2019 CNH Industrial N.V.
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v1.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v20.html
+  *
+  * Contributors:
+  *    Jason Roesbeke - Initial version.
+  *******************************************************************************/
+using System.Collections.Generic;
+using AgGateway.ADAPT.ApplicationDataModel.Shapes;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using WorkRecordPlugin.Utils;
+using NetTopologySuite.Geometries;
+using LineString = GeoJSON.Net.Geometry.LineString;
+using NetTopologySuite.Geometries.Utilities;
+
+namespace WorkRecordPlugin.Mappers.GeoJson
+{
+	public class MultiPolygonMapper
+	{
+		#region Export
+		public static GeoJSON.Net.Geometry.MultiPolygon Map(AgGateway.ADAPT.ApplicationDataModel.Shapes.MultiPolygon multiPolygon, AffineTransformation affineTransformation = null)
+		{
+			var polygons = new List<GeoJSON.Net.Geometry.Polygon>();
+			foreach (var adaptPolygon in multiPolygon.Polygons)
+			{
+				GeoJSON.Net.Geometry.Polygon polygon = PolygonMapper.MapPolygon(adaptPolygon, affineTransformation);
+				if (polygon != null)
+				{
+					polygons.Add(polygon);
+				}
+			}
+
+			if (polygons.Count == 0)
+			{
+				return null;
+			}
+
+			return new GeoJSON.Net.Geometry.MultiPolygon(polygons);
+		}
+		#endregion
+
+		#region Import
+		public static AgGateway.ADAPT.ApplicationDataModel.Shapes.MultiPolygon MapPolygon(GeoJSON.Net.Geometry.MultiPolygon multiPolygonGeoJson, AffineTransformation affineTransformation = null)
+		{
+			var multiPolygon = new AgGateway.ADAPT.ApplicationDataModel.Shapes.MultiPolygon();
+			foreach (var polygonGeoJson in multiPolygonGeoJson.Coordinates)
+			{
+				var polygon = PolygonMapper.MapPolygon(polygonGeoJson, affineTransformation);
+				multiPolygon.Polygons.Add(polygon);
+			}
+
+			return multiPolygon;
+		}
+		#endregion
+	}
+}
